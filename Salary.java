358b43f35f3c0e6e5ae727e9352a35d3f2da6472
glenndsilva1 <glenndsilva@rediffmail.com>
2024-05-30 16:58:38 +0530

    Salary.java

---
+++ Salary.java
@@ -0,0 +1,25 @@
+package temperoralcoupling.test;
+
+public class Salary implements Payment {
+	 public final int amount;
+	 public final Medium<MailInfo> medium;
+	
+	public Salary(int amount){
+	  this(amount, new Email());
+	}
+	
+	public Salary(int amount, Medium<MailInfo> medium){
+		this.amount = amount;
+		this.medium = medium;
+	}
+
+	@Override
+	public void pay() {
+		this.medium.send(new MailInfo());	
+	}
+	
+	public static void main(String args[]) {
+		Payment pymt = new Salary(10);
+		pymt.pay();
+	}
+}
