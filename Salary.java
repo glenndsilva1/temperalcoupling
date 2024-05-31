df8a0ecb1593d5e9256b795f98846715bdad3135
glenndsilva1 <glenndsilva@rediffmail.com>
2024-05-31 10:24:20 +0530

    Salary.java

--- Salary.java
+++ Salary.java
@@ -1,25 +1,26 @@
 package temperoralcoupling.test;
 
-public class DispathSalary implements Payment {
+
+public class Salary implements Payment {
 	 public final int amount;
-	 public final Medium<MailInfo> medium;
-	
-	public DispathSalary(int amount){
-	  this(amount, new Email());
+	 public final Medium medium;
+	 
+	public Salary(int amount){
+	  this(amount, new Email(new MailInfo()));
 	}
 	
-	public DispathSalary(int amount, Medium<MailInfo> medium){
+	public Salary(int amount, Medium medium){
 		this.amount = amount;
 		this.medium = medium;
 	}
 
 	@Override
 	public void payto() {
-		this.medium.send(new MailInfo());	
+		this.medium.send();
 	}
 	
 	public static void main(String args[]) {
-		Payment pymt = new DispathSalary(10);
+		Payment pymt = new Salary(10);
 		pymt.payto();
 	}
 }
