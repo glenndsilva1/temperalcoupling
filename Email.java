1497dfea7c219c0afe722d4ce9c4d25bb1b2012c
glenndsilva1 <glenndsilva@rediffmail.com>
2024-05-31 10:26:06 +0530

    Email.java

--- Email.java
+++ Email.java
@@ -1,9 +1,14 @@
 package temperoralcoupling.test;
 
-public class Email implements Medium<MailInfo>{
+public class Email implements Medium {
+	private final MailInfo m;
+	
+	public Email(MailInfo mailinfo){
+		this.m = mailinfo;
+	}
+	
 	@Override
-	public void send(MailInfo m) {
-		System.out.println("Hey I am send mail to mail info");
-		
+	public void send() {
+		System.out.println("Hey I am send mail to mail info " + m);
 	}
 }
